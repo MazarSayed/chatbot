3b06825a128fa8a5862674bea1af5f60a82db4ed
Mazar Sayed <mohammedmazar11@gmail.com>
2025-03-10 20:29:09 +0530

    logging

--- tools.py
+++ tools.py
@@ -1,48 +1,47 @@
-from src.utils.config import load_config
+#from src.utils.config import load_config
 
-config,prompt = load_config()
+#config,prompt = load_config()
 
 def tools(services,query,history):
-    print(f"\n{'='*50}\n recent history: {history[-2]}\n{'='*50}")
+    recent_history = history[-3:-1]
+    print(f"\n{'='*50}\n recent history: {recent_history}\n{'='*50}")
+
     mytools=[
+       
         {
             "type": "function",
             "function": {
-                "name": "dental_services",
-                "description": """Provides Information only regarding dental services = {} and similar dental services which helps the teeth""".format(services),
+                "name": "business_info",
+                "description": """Provides Information related to Brookline business information and it's dental_services such as - {}, if not in the list of dental_services, let dental_services = 'None',
+                                You need to identify the dental_service and question_describtion based on user_input={} and chat_history={},
+                                Do not call this function if the user wants to book an appointment.""".format(services,query,recent_history),
                 "parameters": {
                     "type": "object",
                     "properties": {
                         "dental_service": {
                             "type": "string",
-                            "description": "Name of the dental service the user is inquring about"
+                            "description": "Name of the dental service the user is inquring about based on user_input and chat_history"
                         },
-                        "user_query": {
+                        "question_describtion": {
                             "type": "string",
-                            "description": "user_query = {} ".format(query)
+                            "description": "Provide a question_describtion based on user_input and chat_history"
                         },
                         
                     },
-                    "required": ["dental_service","user_query"] 
+                    "required": ["dental_service","question_describtion"] 
                 }
             }
         },
-        {
+         {
             "type": "function",
             "function": {
-                "name": "general_question",
-                "description": """Provides infromation regarding general dental services like parking, location, etc""",
+                "name": "book_appointment",
+                "description": """call this function to Book an appointment or consultation with the Brookline Dental Team""",
                 "parameters": {
                     "type": "object",
-                    "properties": {
-                        "user_query": {
-                            "type": "string",
-                            "description": "user_query = {} ".format(query)
-                        }
-                    },
-                    "required": ["user_query"]
+                    "properties": {}
                 }
             }
-        }
+        },
     ]
     return mytools
