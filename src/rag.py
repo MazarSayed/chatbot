a469d5e2c35ae43c654b03fdc4af0eb6dbab4692
Mazar Sayed <mohammedmazar11@gmail.com>
2025-03-17 01:12:46 +0530

    prompt rag

--- src/rag.py
+++ src/rag.py
@@ -52,14 +52,13 @@ def rag(client, query, groq_api_key, current_service, chat_history):
 
                     Follow the steps given below:\n
                         1. Using the Context provide a response only to answer the Question in 3 -4 sentences\n
-                        Note: Do not provide any additional information than requrired for the Question.\n
                         2. Output your response in the structured format with appropriate line breaks and bolds only when needed like a virtual dental concierge.\n
-                        3. If the answer to the Question is not in the Context and the question is regarding the dental clinic & the business:\n
-                           Then provide a tattical response to keep the conversation going and ask the user to contact the front office by filling out the appointment form for more information.\n\n
+                        3. If the answer to the Question is not in the Context:\n
+                           Then provide a tactical response to keep the conversation going and ask the user to contact the front office by filling out the appointment form for more information.\n\n
 
-                    Note: Provide very detailed 6-7 sentence answers for questions only based on treatment plan and after care for the dental services.\n
                     Provide the output by making sure you follow above steps with no additional text.\n
                     Do not provide topics in the response, but structure your response with appropriate line breaks and bolds only when needed like a virtual dental concierge.\n
+                    Note: Provide very detailed 6-7 sentence answers for questions only based on treatment plan and after care for the dental services.\n
                     """
     }
     messages.append(user_message)
